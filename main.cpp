e9abb41194a53c63b7f0c99df9d14dcc20e779cd
vegam <kongkirti@gmail.com>
2025-02-09 23:19:16 +0530

    tried fixing the weird half screen fullscreen toolbar7

--- main.cpp
+++ main.cpp
@@ -3,16 +3,12 @@
 #include <QDebug>
 
 int main(int argc, char *argv[]) {
-    // Force X11 backend
     qputenv("QT_QPA_PLATFORM", "xcb");
     
-    // Force GStreamer backend
     qputenv("QT_MEDIA_BACKEND", "gstreamer");
     
-    // Additional GStreamer debug
     qputenv("GST_DEBUG", "2");
     
-    // Use desktop OpenGL with NVIDIA
     QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
     
     QApplication app(argc, argv);
